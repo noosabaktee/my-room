58123e0a280b02ddd5279071a9d9a8bde6eecdd3
noosabaktee <ramanusabakti03@gmail.com>
2024-05-07 21:32:00 +0700

    Finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import { Canvas } from '@react-three/fiber'
 import Model from './Model'
-import { Html  } from '@react-three/drei'
 import { useState } from 'react';
-import * as THREE from 'three';
 import './App.css'
 
 import { Camera } from './Camera';
 
 
 function App() {
+  const d = new Date();
+  let year = d.getFullYear();
   const body = [
-    {title: "Hello World", content:"Are you ready?"},
-    {title: "Hello World", content:"Are you ready 2?"},
-    {title: "Hello World", content:"Are you ready 3?"},
-    {title: "Hello World", content:"Are you ready?"},
-    {title: "Hello World", content:"Are you ready?"},
-    {title: "Hello World", content:"Are you ready?"},
-    {title: "Hello World", content:"Are you ready?"},
-    {title: "Hello World", content:"Are you ready?"}
+    {title: "Hello World!", content:"Are you ready? Turn on the light!"},
+    {title: "Hi There!", content:"How are you?"},
+    {title: "I'm Rama a.k.a Ramben", content:"I was born " + (year-2004).toString() + " years ago."},
+    {title: "Breaking News!!!", content:"This newspaper describes me."},
+    {title: "Start Now", content:"If you want to be successful, do something!"},
+    {title: "My Projects", content:"These are all projects created by me. Click there!"},
+    {title: "Windows XP", content:"This is my first windows."},
+    {title: "Follow Me", content:"Click there if you are interested."}
   ]
 
   const [pos, setPos] = useState(0)
@@ -36,6 +36,7 @@ function App() {
       setIsTurnOn(false)
     }
   }
+  
   return (
     <div id="App">
       <div id="canvas">
@@ -48,16 +49,12 @@ function App() {
       </div>
       <div id="content">
         <h1>{body[pos].title}</h1>
-        <p>{body[pos].content}</p>
-        <div>
-          {pos > 0 &&
-            <button onClick={() => {setPos(pos-1)}}>Back</button>
-          }
-          {pos < 7 &&
-            <button onClick={() => {setPos(pos+1)}}>Next</button>
-          }
+        <p style={{fontSize: "20px"}}>{body[pos].content}</p>
+        <div style={{display: "flex"}}>
+            <button disabled={pos <= 0} onClick={() => {setPos(pos-1)}}><span className="material-symbols-outlined">arrow_left_alt</span> Back</button>
+            <button disabled={pos >= 7} onClick={() => {setPos(pos+1)}} style={{marginLeft: "10px"}}>Next <span className="material-symbols-outlined">arrow_right_alt</span></button>
         </div>
-        <button onClick={turnOn}>Turn {isTurnOn ? 'Off' : 'On'}</button>
+        <button onClick={turnOn}>Turn {isTurnOn ? 'Off' : 'On'} Light <span className="material-symbols-outlined">emoji_objects</span></button>
       </div>
     </div>
   )
--- src/Model.js
+++ src/Model.js
@@ -4,14 +4,18 @@ import * as THREE from 'three';
 import { Html  } from '@react-three/drei'
 
 const Model = () => {
+  function loaded(){
+    document.querySelector("#loading").style.display = "none"
+    document.querySelector("#xp").style.display = "block"
+  }
   const gltf = useLoader(GLTFLoader, '/scene.gltf')
   return (
     <>
       <primitive object={gltf.scene} >
         <Html transform distanceFactor={1} position={[-3.13, 2.56, -0.946]} scale={0.2} rotation-y={THREE.MathUtils.degToRad(90)}>
           <div>
-            {/* <iframe src='https://pranx.com/windows-xp-simulator/' style={{borderRadius: "30px"}}  width={1090} height={770}></iframe> */}
-            <img src='loading_xp.gif' width={1090} height={770} style={{borderRadius: "30px"}} />
+            <iframe id='xp' onLoad={loaded} src='https://winxp.vercel.app/' style={{borderRadius: "30px",display:"none"}}  width={1090} height={770}></iframe>
+            <img src='loading_xp.gif' width={1090} height={770} style={{borderRadius: "30px"}} id='loading'/>
           </div>
         </Html>
         <Html transform distanceFactor={1} position={[-4, 4.4, 0]} scale={1.1} rotation-y={THREE.MathUtils.degToRad(90)}>
@@ -32,6 +36,13 @@ const Model = () => {
                 <a href='https://ramanusabakti.itch.io' target='_blank' className='porto'></a>
                 <a href='https://tausos.netlify.app' target='_blank' className='porto'></a>
               </div>
+            </div>
+        </Html>
+        <Html transform distanceFactor={1} position={[0, 2, -3.5]} rotation-x={THREE.MathUtils.degToRad(-90)} rotation-y={THREE.MathUtils.degToRad(0)} rotation-z={THREE.MathUtils.degToRad(340)}>
+            <div style={{marginTop: "210px",paddingLeft:"20px"}}>
+              <a href='https://www.instagram.com/noosabaktee/' target='_blank' style={{marginBottom: "15px",display: "flex",width: "70px",height:"20px"}}></a>
+              <a href='https://github.com/noosabaktee/' target='_blank' style={{marginBottom: "15px",display: "flex",width: "70px",height:"20px"}}></a>
+              <a href='https://www.linkedin.com/in/rama-nusa-bakti-8a9410288/' target='_blank' style={{marginBottom: "15px",display: "flex",width: "70px",height:"20px"}}></a>
             </div>
         </Html>
       </primitive>
